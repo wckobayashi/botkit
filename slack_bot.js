f233d7a7b244ceb746365f4e39ada47b2cbb2a79
unknown <dota.kobayashi@W-723.jpn.webcrew.local>
2016-07-06 14:08:41 +0900

    試しに為替返すやつ追加

--- slack_bot.js
+++ slack_bot.js
@@ -244,3 +244,19 @@ function formatUptime(uptime) {
     uptime = uptime + ' ' + unit;
     return uptime;
 }
+
+controller.hears(['(usd|eur|gbp|aud|nzd|cad|chf)'],'direct_mention',function    (bot, message) {
+  var matches = message.text.match(/(usd|eur|gbp|aud|nzd|cad|chf)/i);
+  var cur = matches[0].toUpperCase() + "JPY";
+  var request = require( 'request' );
+  url = 'http://fx.mybluemix.net/';
+  request( url, function( error, response, body ){
+    if( !error && response.statusCode == 200 ){
+      var json = JSON.parse( body );
+      var dt = json['datetime'];
+      var rate = json['rate'];
+      var p = rate[cur];
+      bot.reply( message, dt + ' ' + p );
+    }
+  });
+});
